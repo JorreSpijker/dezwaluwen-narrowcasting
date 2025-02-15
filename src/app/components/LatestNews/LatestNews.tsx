38e7fd9432690e71f5417c03112e7e32070a5f03
Jorre Spijker <jorre@linku.nl>
2025-02-15 20:16:54 +0100

    Update LatestNews component to depend on fetchUrl; comment out unused imports and code in GeneralPage

--- src/app/components/LatestNews/LatestNews.tsx
+++ src/app/components/LatestNews/LatestNews.tsx
@@ -29,7 +29,7 @@ export default function LatestNews() {
         setLoading(false);
         console.error('Error fetching program:', error)
       });
-  }, []);
+  }, [fetchUrl]);
 
   return (
     <div className="bg-white rounded-xl shadow-lg p-6 w-full h-full">
--- src/app/general/page.tsx
+++ src/app/general/page.tsx
@@ -4,8 +4,8 @@ import LatestResults from "../components/LatestResults/LatestResults";
 import Program from "../components/ProgramAllTeams/ProgramAllTeams";
 import LatestNews from "../components/LatestNews/LatestNews";
 import Alert from "../components/Alert/Alerts";
-import { useEffect, useState } from "react";
-import Airtable from "airtable";
+// import { useEffect } from "react";
+// import Airtable from "airtable";
 
 // https://api-mijn.korfbal.nl/api/v2/clubs/NCX35M2
 // https://api-mijn.korfbal.nl/api/v2/clubs/NCX35M2/program
@@ -13,49 +13,49 @@ import Airtable from "airtable";
 // https://api-mijn.korfbal.nl/api/v2/clubs/NCX35M2/teams
 // https://www.dezwaluwen.nl/?rest_route=/wp/v2/posts/
 
-interface AirtableRecord {
-  id: string;
-  fields: {
-    show: boolean;
-    label: string;
-    message: string;
-  };
-}
+// interface AirtableRecord {
+//   id: string;
+//   fields: {
+//     show: boolean;
+//     label: string;
+//     message: string;
+//   };
+// }
 
 export default function GeneralPage() {
   // const [airtableData, setAirtableData] = useState<AirtableRecord[]>([]);
 
-  useEffect(() => {
-    if (!process.env.AIRTABLE_API || !process.env.AIRTABLE_BASE) {
-      console.error("AIRTABLE_API is not set");
-      return;
-    }
+  // useEffect(() => {
+  //   if (!process.env.AIRTABLE_API || !process.env.AIRTABLE_BASE) {
+  //     console.error("AIRTABLE_API is not set");
+  //     return;
+  //   }
 
-    const base = new Airtable({ apiKey: process.env.AIRTABLE_API }).base(`${process.env.AIRTABLE_BASE}`);
+    // const base = new Airtable({ apiKey: process.env.AIRTABLE_API }).base(`${process.env.AIRTABLE_BASE}`);
 
-    base('messages').select({
-      // Selecting the first 3 records in Grid view:
-      maxRecords: 1,
-    }).eachPage((records, fetchNextPage) => {
-      // This function (`page`) will get called for each page of records.
-      const mappedRecords = records.map(record => ({
-        id: record.id,
-        fields: record.fields as {
-          show: boolean;
-          label: string;
-          message: string;
-        }
-      }));
-      setAirtableData(mappedRecords);
+    // base('messages').select({
+    //   // Selecting the first 3 records in Grid view:
+    //   maxRecords: 1,
+    // }).eachPage((records, fetchNextPage) => {
+    //   // This function (`page`) will get called for each page of records.
+    //   const mappedRecords = records.map(record => ({
+    //     id: record.id,
+    //     fields: record.fields as {
+    //       show: boolean;
+    //       label: string;
+    //       message: string;
+    //     }
+    //   }));
+      // setAirtableData(mappedRecords);
 
       // To fetch the next page of records, call `fetchNextPage`.
       // If there are more records, `page` will get called again.
       // If there are no more records, `done` will get called.
-      fetchNextPage();
-    }, (err) => {
-      if (err) { console.error(err); return; }
-    });
-  }, []);
+  //     fetchNextPage();
+  //   }, (err) => {
+  //     if (err) { console.error(err); return; }
+  //   });
+  // }, []);
 
   return (
     <div className="grid grid-cols-2 gap-8 max-w-7xl lg:w-full px-4 lg:mx-auto">
